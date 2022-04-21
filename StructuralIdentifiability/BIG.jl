259795e3d8991a4d73976d713939471911af0fea
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-04-21 17:00:47 +0200

    New models

---
+++ StructuralIdentifiability/BIG.jl
@@ -0,0 +1,27 @@
+# Diane T Finegood, Luisa Scaglia, and Susan Bonner-Weir. Dynamics of β-cell mass in the growing rat
+# pancreas: estimation with a simple mathematical model. Diabetes, 44(3):249–256, 1995
+
+using Logging
+
+using StructuralIdentifiability
+
+logger = Logging.SimpleLogger(stdout, Logging.Info)
+global_logger(logger)
+
+ode = @ODEmodel(
+    G'(t) = u1(t)-(c+si*II(t))*G(t),
+    beta'(t) = beta(t)* ( ( (1/(48*24*60)) /(1+((8/G(t))^2)) ) - ( (1/(40*24*60))/(1+((G(t)/5)^8)))),
+    II'(t) = p1*beta(t)*(G(t)^2/(alpha^2+G(t)^2)) - gm*II(t),
+    y1(t) = G(t)
+)
+
+@time println(assess_identifiability(ode))
+
+
+
+
+
+
+
+
+
