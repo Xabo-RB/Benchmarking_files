8c097df670c96257e30daf8e5a63c462a0fe9f42
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-05-03 13:16:55 +0200

    Update c2m.jl

--- StructuralIdentifiability/c2m.jl
+++ StructuralIdentifiability/c2m.jl
@@ -15,3 +15,19 @@ ode = @ODEmodel(
 )
 
 @time println(assess_identifiability(ode))
+
+    
+using Logging
+
+using StructuralIdentifiability
+
+logger = Logging.SimpleLogger(stdout, Logging.Info)
+global_logger(logger)
+
+ode = @ODEmodel(
+    x1'(t) = -(k12 + k1e)*x1(t) + k21*x2(t) + b*u,
+    x2'(t) = k12 * x1(t) - k21 * x2(t),
+    y1(t) = x1
+)
+
+@time println(assess_identifiability(ode))
