135a2b722a30dea2e0a653ea4bb976f0bbdaca88
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-05-12 19:21:22 +0200

    Update cholera.jl

--- SIAN julia/cholera.jl
+++ SIAN julia/cholera.jl
@@ -14,6 +14,6 @@ ode = @ODEmodel(
     y2(t) = S(t)+I(t)+R(t)
 )
 
-@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, nthrds = 1))
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
 
-println(res)
+# @time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 0, infolevel = 10, nthrds = 1))
