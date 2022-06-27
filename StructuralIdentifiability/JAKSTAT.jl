fc9722e5de600e0e3ad243727c34aeac0cb5b3d0
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-06-27 11:26:41 +0200

    Create JAKSTAT.jl

---
+++ StructuralIdentifiability/JAKSTAT.jl
@@ -0,0 +1,39 @@
+#--------------------------------------------------------------------------
+# Raia V et al. (2011) Dynamic mathematical modeling of IL13-induced 
+# signaling in Hodgkin and primary mediastinal B-cell lymphoma allows 
+# prediction of therapeutic targets. 
+# Cancer Research 71(3):693{704.
+#--------------------------------------------------------------------------
+
+
+
+using Logging
+
+using StructuralIdentifiability
+
+logger = Logging.SimpleLogger(stdout, Logging.Info)
+global_logger(logger)
+
+ode = @ODEmodel(
+    x1'(t) = -t1*x1(t)*c1*u(t) - t5*x1(t) + t6*x2(t),
+    x2'(t) = t5*x1(t)-t6*x2(t),
+    x3'(t) = t1*c1*u(t)*x1(t)-t2*x3(t)*(-x6(t)+2.8),
+    x4'(t) = t2*x3(t)*(-x6(t)+2.8) - t3*x4(t),
+    x5'(t) = t3*x4(t)-t4*x5(t),
+    x6'(t) = -t7*x3(t)*x6(t)/(1+t13*x1(t))-t7*x4(t)*x6(t)/(1+t13*x10(t))+t8*(-x6(t)+2.8)*c2,
+    x7'(t) = -t9*x7(t)*(-x6(t)+2.8)+t10*(-x7(t)+165)*c2,
+    x8'(t) = t11*(-x7(t)+165),
+    x9'(t) = -t12*c1*u(t)*x9(t),
+    x10'(t) = x8(t)*t14/(t15+x8(t))-t16*x10(t),
+    y1(t) = x1(t)+x3(t)+x4(t),
+    y2(t) = t18*(x3(t) + x4(t) + x5(t) +(0.34-x9(t))),
+    y3(t) = t19*(x4(t) + x5(t)),
+    y4(t) = t20*(-x6(t)+2.8),
+    y5(t) = t21*x8(t),
+    y6(t) = t22*x8(t)*t17/t11,
+    y7(t) = x10(t),
+    y8(t) = -x7(t)+165
+)
+
+
+@time println(assess_identifiability(ode))
