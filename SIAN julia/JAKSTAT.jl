7c10a3ee40a57f1dff6ce7e243208c2825300bc7
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-06-27 11:26:02 +0200

    Update JAKSTAT.jl

--- SIAN julia/JAKSTAT.jl
+++ SIAN julia/JAKSTAT.jl
@@ -30,3 +30,5 @@ ode = @ODEmodel(
     y7(t) = x10(t),
     y8(t) = -x7(t)+165
 )
+
+@time println(identifiability_ode(ode, get_parameters(ode); p = 0.99, p_mod = 2^29 - 3, infolevel = 10, nthrds = 1))
