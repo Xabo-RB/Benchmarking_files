239764bc5b50614f3f6a9b63b9575c44077b9085
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-06-27 12:21:21 +0200

    Update JAKSTAT.jl

--- StructuralIdentifiability/JAKSTAT.jl
+++ StructuralIdentifiability/JAKSTAT.jl
@@ -15,15 +15,15 @@ logger = Logging.SimpleLogger(stdout, Logging.Info)
 global_logger(logger)
 
 ode = @ODEmodel(
-    x1'(t) = -t1*x1(t)*c1*u(t) - t5*x1(t) + t6*x2(t),
+    x1'(t) = -t1*x1(t)*2.265*u(t) - t5*x1(t) + t6*x2(t),
     x2'(t) = t5*x1(t)-t6*x2(t),
-    x3'(t) = t1*c1*u(t)*x1(t)-t2*x3(t)*(-x6(t)+2.8),
+    x3'(t) = t1*2.265*u(t)*x1(t)-t2*x3(t)*(-x6(t)+2.8),
     x4'(t) = t2*x3(t)*(-x6(t)+2.8) - t3*x4(t),
     x5'(t) = t3*x4(t)-t4*x5(t),
     x6'(t) = -t7*x3(t)*x6(t)/(1+t13*x1(t))-t7*x4(t)*x6(t)/(1+t13*x10(t))+t8*(-x6(t)+2.8)*c2,
     x7'(t) = -t9*x7(t)*(-x6(t)+2.8)+t10*(-x7(t)+165)*c2,
     x8'(t) = t11*(-x7(t)+165),
-    x9'(t) = -t12*c1*u(t)*x9(t),
+    x9'(t) = -t12*2.265*u(t)*x9(t),
     x10'(t) = x8(t)*t14/(t15+x8(t))-t16*x10(t),
     y1(t) = x1(t)+x3(t)+x4(t),
     y2(t) = t18*(x3(t) + x4(t) + x5(t) +(0.34-x9(t))),
