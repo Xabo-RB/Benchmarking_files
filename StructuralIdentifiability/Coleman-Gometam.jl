4245c09736b81cbd8e446df6c5d17d30cb48faba
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-07-18 15:33:47 +0200

    Update Coleman-Gometam.jl

--- StructuralIdentifiability/Coleman-Gometam.jl
+++ StructuralIdentifiability/Coleman-Gometam.jl
@@ -8,8 +8,8 @@ logger = Logging.SimpleLogger(stdout, Logging.Info)
 global_logger(logger)
 
 ode = @ODEmodel(
-    x1'(t) = x1*(a1-b11*ln(x1(t))-b12*ln(x2(t))),
-    x2'(t) = x2(t)*(a2-b21*ln(x1(t))-b22*ln(x2(t))),
+    x1'(t) = x1*(a1-b11*log(x1(t))-b12*log(x2(t))),
+    x2'(t) = x2(t)*(a2-b21*log(x1(t))-b22*log(x2(t))),
     y1(t) = x1
 )
 
