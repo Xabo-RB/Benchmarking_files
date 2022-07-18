138f24cd8ffb63753150911ad11dc1a780df4ca6
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-07-18 16:15:08 +0200

    Update Coleman-Gometam.py

--- StrikePy/Coleman-Gometam.py
+++ StrikePy/Coleman-Gometam.py
@@ -1,17 +1,16 @@
-#Alejandro F Villaverde, Neil D Evans, Michael J Chappell, and Julio R Banga. Input-dependent
-#structural identifiability of nonlinear systems. IEEE Control Systems Letters, 3(2):272–277, 2018
 
 import sympy as sym
+from math import log
 
 # 2 states
 x1 = sym.Symbol('x1')  # define the symbolic variable x1
 x2 = sym.Symbol('x2')  # define the symbolic variable x2
-x3 = sym.Symbol('a1')  # define the symbolic variable x3
-x4 = sym.Symbol('a2')  # define the symbolic variable x4
-x5 = sym.Symbol('b11')  # define the symbolic variable x5
-x6 = sym.Symbol('b12')  # define the symbolic variable x6
-x6 = sym.Symbol('b21')  # define the symbolic variable x7
-x6 = sym.Symbol('b22')  # define the symbolic variable x8
+a1 = sym.Symbol('a1')  # define the symbolic variable x3
+a2 = sym.Symbol('a2')  # define the symbolic variable x4
+b11 = sym.Symbol('b11')  # define the symbolic variable x5
+b12 = sym.Symbol('b12')  # define the symbolic variable x6
+b21 = sym.Symbol('b21')  # define the symbolic variable x7
+b22 = sym.Symbol('b22')  # define the symbolic variable x8
 
 x = [[x1], [x2]]
 
@@ -29,7 +28,7 @@ w = []
 p = [[a1], [b11], [b12], [b21], [a2], [b22]]
 
 # dynamic equations
-f = [[x1*(a1-b11*ln(x1)-b12*ln(x2))], [x2*(a2-b21*ln(x1)-b22*ln(x2))]]
+f = [[x1*(a1-b11*log(x1)-b12*log(x2))], [x2*(a2-b21*log(x1)-b22*log(x2))]]
 
 
 variables_locales = locals().copy()
