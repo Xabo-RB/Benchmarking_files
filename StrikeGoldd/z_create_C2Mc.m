bd154a7e64c9ce983575ca2442f17659f38ddf5f
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-07-21 12:07:32 +0200

    Update z_create_C2Mc.m

--- StrikeGoldd/z_create_C2Mc.m
+++ StrikeGoldd/z_create_C2Mc.m
@@ -14,6 +14,7 @@ h = x1;
 % one input
 syms u1;
 u = [];
+w = u1;
 
 % 4 unknown parameters 
 syms x3 x4 x5 x6
@@ -27,5 +28,5 @@ f = [-(x3+x4)*x1+x5*x2+x6*u1;
 ics  = []; 
 known_ics = [0,0];
 
-save('C2M','x','p','h','f','u','ics','known_ics');
+save('C2M','x','p','h','f','u','w','ics','known_ics');
 
