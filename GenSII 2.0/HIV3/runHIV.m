2c84bc5e76ca27951a0045bcfa091035d1a80400
Xabier Rey Barreiro <xabo.rey@gmail.com>
2022-11-17 13:07:43 +0100

    Update runHIV.m

--- GenSII 2.0/HIV3/runHIV.m
+++ GenSII 2.0/HIV3/runHIV.m
@@ -8,7 +8,7 @@
 % Confirm execution
 %genssiAskForConfirmation(1);
 
-syms xx0 y0 v0 ww0 z0
+%syms xx0 y0 v0 ww0 z0
+%genssiMain('HIV',15,[xx0;y0;v0;ww0;z0]);
 
-
-genssiMain('HIV',15,[xx0;y0;v0;ww0;z0]);
+genssiMain('HIV',15);
