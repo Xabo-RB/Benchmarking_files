7d148fb5aa9b103d8f4c783ac1d908d0088cb38f
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-02-22 13:19:41 +0100

    Update runBIG.m

--- GenSII 2.0/BIG/runBIG.m
+++ GenSII 2.0/BIG/runBIG.m
@@ -1,9 +1,7 @@
 % runHIV runs the structural identifiability analysis for the 
 % model of HIV dynamics described by
 % 
-%    Meshkat et al. (2014). On finding and using identifiable parameter
-%    combinations in nonlinear dynamic Systems Biology models and
-%    COMBOS: a novel Web implementation, PLoS ONE, 9, e110261.
+
 
 % Confirm execution
 genssiAskForConfirmation(1);
