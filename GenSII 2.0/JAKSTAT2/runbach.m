eca549104871c5dbc71d7587d5457ba3ce2b6bf3
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-02-22 13:21:21 +0100

    Update runbach.m

--- GenSII 2.0/JAKSTAT2/runbach.m
+++ GenSII 2.0/JAKSTAT2/runbach.m
@@ -1,9 +1,3 @@
-% runNFkB runs the structural identifiability analysis for model of NFkB
-% signalling introduced by
-% 
-%    Lipniacki et al. (2004). Mathematical model of NFkB regulatory  
-%    module, Journal of Theoretical Biology 228, 195-215.
-
 % Symbolic parameters for identifiability analysis
 
 % Confirm execution
