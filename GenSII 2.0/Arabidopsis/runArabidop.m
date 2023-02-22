222546a3631b65bb8c01c38df6c3230c87fcb6e1
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-02-22 13:19:27 +0100

    Update runArabidop.m

--- GenSII 2.0/Arabidopsis/runArabidop.m
+++ GenSII 2.0/Arabidopsis/runArabidop.m
@@ -1,9 +1,7 @@
 % runHIV runs the structural identifiability analysis for the 
 % model of HIV dynamics described by
 % 
-%    Meshkat et al. (2014). On finding and using identifiable parameter
-%    combinations in nonlinear dynamic Systems Biology models and
-%    COMBOS: a novel Web implementation, PLoS ONE, 9, e110261.
+
 
 % Symbolic parameters for identifiability analysis
 syms p1 p2 p5 p8 p10 p11 p12 p15 p18 p19 p26 p27
