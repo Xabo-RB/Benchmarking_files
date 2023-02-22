6e902355ecc0157753381e2721da3279a4a5a68e
Xabier Rey Barreiro <xabo.rey@gmail.com>
2023-02-22 13:21:34 +0100

    Update runMAPK.m

--- GenSII 2.0/MAPK/runMAPK.m
+++ GenSII 2.0/MAPK/runMAPK.m
@@ -1,10 +1,3 @@
-% runHIV runs the structural identifiability analysis for the 
-% model of HIV dynamics described by
-% 
-%    Meshkat et al. (2014). On finding and using identifiable parameter
-%    combinations in nonlinear dynamic Systems Biology models and
-%    COMBOS: a novel Web implementation, PLoS ONE, 9, e110261.
-
 % Confirm execution
 %genssiAskForConfirmation(1);
 syms k1 k2 k3 k4 k5 k6 ...
